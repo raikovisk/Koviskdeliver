aeb8b848c7414b874dd7960779fdf1220a9ece3b
Raisson Almeida <almeidaraisson@gmail.com>
2022-07-22 14:01:46 -0300

    Project created

---
+++ backend/src/main/java/com/koviskdev/koviskdelivery/KoviskdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.koviskdev.koviskdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoviskdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoviskdeliveryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/koviskdev/koviskdelivery/KoviskdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koviskdev.koviskdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoviskdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
